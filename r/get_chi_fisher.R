fd84fee7260f0b45f844d25f08faa22439dabab8
Morrigan Mahady <morri.mahady@gmail.com>
2025-03-07 15:16:41 -0600

    Fix invalid processing, update parameters and validations
    Adds n_decimal parameter. Updates parameter checks for parameters that could be lists of column names (would throw error with missing checks). Update chisq and fisher to use a data.table, not x= y= vector pass (was giving invalid calculations. Set fisher to simulate p value with a set random seed, to avoid hang on large sets. Updates format of output digits.

--- r/get_chi_fisher.R
+++ r/get_chi_fisher.R
@@ -18,9 +18,8 @@
 #' @param .value_var Column that contains the values or levels in .df. Used 
 #'      to subset .df when processing omissions. Default is NA, which
 #'      skips sensitivity testing for omitting levels.
-#' @param .omit_vals A vector or list that gives the values or levels in 
-#'      .df[[.value_var]] to omit in sensitivity testing. Default is NA, which
-#'      skips sensitivity testing for omitting levels.
+#' @param n_decimal Number of decimal places to display in values. Default
+#'   value is 5 
 #'
 #' @return A data frame
 #' @export
@@ -44,14 +43,18 @@
 #'  .counts_a_var = 'AM_0',
 #'  .counts_b_var = 'AM_1',
 #'  .value_var = 'cyl',
-#'  .omit_vals = '4'
+#'  .omit_vals = '4',
+#'  n_decimal = 8
 #'  )
 #'
 
 get_chi_fisher <- function(
     .df, .counts_a_var, .counts_b_var, .probs_b_var = NA_character_, 
-    .value_var = NA_character_, .omit_vals = NA
+    .value_var = NA_character_, .omit_vals = NA, n_decimal = 5
 ){
+  # Set a random seed, as fisher test uses "simulate.p.value" to avoid hanging
+  # on large sets
+  set.seed(42)
   # Parse Inputs
   # =========================================================================
   # Ensure .df is a data frame
@@ -65,37 +68,40 @@ get_chi_fisher <- function(
   }
   # Validate if .counts_a_var is reasonable
   # -------------------------------------------------------------------------
-  # If .counts_a_var is given, make sure it was given as a string
-  if (!is.na(.counts_a_var) & !is.character(.counts_a_var)){
-    message <- paste(
-      "Parameter .counts_a_var requires a character column/vector name.", 
-      "Check input and try again."
-    )
-    stop(message)
-  }
+  # If .counts_a_var is given...
+  if(sum(!is.na(.counts_a_var)) > 0){
+  ## make sure it was given as a string
+    if (!is.character(.counts_a_var)){
+      message <- paste(
+        "Parameter .counts_a_var requires a character column/vector name.", 
+        "Check input and try again."
+      )
+      stop(message)
+    }
   # If there's more than one value for .counts_a_var, raise an stop
-  if (!is.na(.counts_a_var) & length(.counts_a_var) > 1) {
-    message <- paste(
-      "Multiple values in .counts_a_var argument. Only one column allowed.", 
-      "Check input and try again."
-    )
-    stop(message)
-  }
+    if (length(.counts_a_var) > 1) {
+      message <- paste(
+        "Multiple values in .counts_a_var argument. Only one column allowed.", 
+        "Check input and try again."
+      )
+      stop(message)
+    }
   # If .counts_a_var was given, make sure it exists in .df
-  if(!is.na(.counts_a_var) & !(.counts_a_var %in% colnames(.df))){
-    message <- paste0(
-      .counts_a_var, " was given for .counts_a_var, but it is not in .df! ",
-      "Check input and try again."
-    )
-    stop(message)
-  }
+    if(!(.counts_a_var %in% colnames(.df))){
+      message <- paste0(
+        .counts_a_var, " was given for .counts_a_var, but it is not in .df! ",
+        "Check input and try again."
+      )
+      stop(message)
+    }
   # If .counts_a_var was given, be sure it's numeric.
-  if (!is.na(.counts_a_var) & !is.numeric(.df[[.counts_a_var]])) {
-    message <- paste(
-      ".counts_a_var must be numeric, but it is not.", 
-      "Check input and try again."
-    )
-    stop(message)
+    if (!is.numeric(.df[[.counts_a_var]])) {
+      message <- paste(
+        ".counts_a_var must be numeric, but it is not.", 
+        "Check input and try again."
+      )
+      stop(message)
+    }
   }
   # If .counts_a_var does not exist, raise an stop
   if (is.na(.counts_a_var)) {
@@ -107,37 +113,40 @@ get_chi_fisher <- function(
   }
   # Validate if .counts_b_var is reasonable
   # -------------------------------------------------------------------------
-  # If .counts_b_var is given, make sure it was given as a string
-  if (!is.na(.counts_b_var) & !is.character(.counts_b_var)){
-    message <- paste(
-      "Parameter .counts_b_var requires a character column/vector name.", 
-      "Check input and try again."
-    )
-    stop(message)
-  }
-  # If there's more than one value for .counts_b_var, raise an stop
-  if (!is.na(.counts_b_var) & length(.counts_b_var) > 1) {
-    message <- paste(
-      "Multiple values in .counts_b_var argument. Only one column allowed.", 
-      "Check input and try again."
-    )
-    stop(message)
-  }
-  # If .counts_b_var was given, make sure it exists in .df
-  if(!is.na(.counts_b_var) & !(.counts_b_var %in% colnames(.df))){
-    message <- paste0(
-      .counts_b_var, " was given for .counts_b_var, but it is not in .df! ",
-      "Check input and try again."
-    )
-    stop(message)
-  }
-  # If .counts_b_var was given, be sure it's numeric.
-  if (!is.na(.counts_b_var) & !is.numeric(.df[[.counts_b_var]])) {
-    message <- paste(
-      ".counts_b_var must be numeric, but it is not.", 
-      "Check input and try again."
-    )
-    stop(message)
+  # If .counts_b_var is given...
+  if (sum(!is.na(.counts_b_var))>0){
+    ## make sure it was given as a string
+    if(!is.character(.counts_b_var)){
+      message <- paste(
+        "Parameter .counts_b_var requires a character column/vector name.", 
+        "Check input and try again."
+      )
+      stop(message)
+    }
+    # If there's more than one value for .counts_b_var, raise an stop
+    if (length(.counts_b_var) > 1) {
+      message <- paste(
+        "Multiple values in .counts_b_var argument. Only one column allowed.", 
+        "Check input and try again."
+      )
+      stop(message)
+    }
+    # If .counts_b_var was given, make sure it exists in .df
+    if(!(.counts_b_var %in% colnames(.df))){
+      message <- paste0(
+        .counts_b_var, " was given for .counts_b_var, but it is not in .df! ",
+        "Check input and try again."
+      )
+      stop(message)
+    }
+    # If .counts_b_var was given, be sure it's numeric.
+    if (!is.numeric(.df[[.counts_b_var]])) {
+      message <- paste(
+        ".counts_b_var must be numeric, but it is not.", 
+        "Check input and try again."
+      )
+      stop(message)
+    }
   }
   # If .counts_b_var does not exist, raise an stop
   if (is.na(.counts_b_var)) {
@@ -158,41 +167,46 @@ get_chi_fisher <- function(
   }
   # Check .probs_b_var, if given. Remove if invalid
   # -------------------------------------------------------------------------
-  # If .probs_b_var is given, make sure it was given as a string
-  if (!is.na(.probs_b_var) & !is.character(.probs_b_var)){
+  # If .probs_b_var is given... 
+  if (sum(!is.na(.probs_b_var)) > 0 ){
+    # make sure it was given as a string
+    if(!is.character(.probs_b_var)){
     message <- paste(
       "Parameter .probs_b_var requires a character column/vector name.", 
       "Reverting to NA."
     )
     .probs_b_var = NA_character_
     warning(message)
-  }
-  # If there's more than one value for .probs_b_var, remove them all
-  if (!is.na(.probs_b_var) & length(.probs_b_var) > 1) {
-    message <- paste(
-      "Multiple values in .probs_b_var argument. Only one column allowed.", 
-      "Reverting to NA."
-    )
-    .probs_b_var = NA_character_
-    warning(message)
-  }
-  # If .probs_b_var was given, make sure it exists in .df
-  if(!is.na(.probs_b_var) & !(.probs_b_var %in% colnames(.df))){
-    message <- paste0(
-      .probs_b_var, " was given for .probs_b_var, but it is not in .df! ",
-      "Reverting to NA."
-    )
-    .probs_b_var = NA_character_
-    warning(message)
-  }
-  # If .probs_b_var was given, be sure it's numeric.
-  if (!is.na(.probs_b_var) & !is.numeric(.df[[.probs_b_var]])) {
-    message <- paste(
-      ".probs_b_var must be numeric, but it is not.", 
-      "Reverting to NA."
-    )
-    .probs_b_var = NA_character_
-    warning(message)
+    }
+    # If there's more than one value for .probs_b_var, remove them all
+    if (length(.probs_b_var) > 1) {
+      message <- paste(
+        "Multiple values in .probs_b_var argument. Only one column allowed.", 
+        "Reverting to NA."
+      )
+      .probs_b_var = NA_character_
+      warning(message)
+    }
+    # If .probs_b_var was given, make sure it exists in .df
+    if(!(.probs_b_var %in% colnames(.df))){
+      message <- paste0(
+        .probs_b_var, " was given for .probs_b_var, but it is not in .df! ",
+        "Reverting to NA."
+      )
+      .probs_b_var = NA_character_
+      warning(message)
+    }
+    # If .probs_b_var was given, be sure it's numeric.
+    if (!is.na(.probs_b_var)) {
+      if(!is.numeric(.df[[.probs_b_var]])){
+        message <- paste(
+          ".probs_b_var must be numeric, but it is not.", 
+          "Reverting to NA."
+        )
+        .probs_b_var = NA_character_
+        warning(message)
+      }
+    }
   }
   # Chi-Square integer checks
   # ------------------------------------------------------------------------
@@ -221,40 +235,42 @@ get_chi_fisher <- function(
   }
   # Validate if .value_var is reasonable
   # -------------------------------------------------------------------------
-  # If .value_var is given, make sure it was given as a string pr ignore it
-  if (!is.na(.value_var) & !is.character(.value_var)){
-    message <- paste(
-      "Parameter .value_var requires a character column/vector name.", 
-      "Ignoring input. If .omit_vals was also given, it's also ignored."
-    )
-    .value_var = NA_character_
-    .omit_vals = NA
-    warning(message)
-  }
+  # If .value_var is given... make sure it was given as a string pr ignore it
+  if (sum(!is.na(.value_var)) > 0) {
+    if(!is.character(.value_var)){
+      message <- paste(
+        "Parameter .value_var requires a character column/vector name.", 
+        "Ignoring input. If .omit_vals was also given, it's also ignored."
+      )
+      .value_var = NA_character_
+      .omit_vals = NA
+      warning(message)
+    }
   # If there's more than one value for .value_var, raise an warning and ignore
-  if (!is.na(.value_var) & length(.value_var) > 1) {
-    message <- paste(
-      "Multiple values in .value_var argument. Only one column allowed.", 
-      "Ignoring input. If .omit_vals was also given, it's also ignored."
-    )
-    .value_var = NA_character_
-    .omit_vals = NA
-    warning(message)
-  }
+    if (length(.value_var) > 1) {
+      message <- paste(
+        "Multiple values in .value_var argument. Only one column allowed.", 
+        "Ignoring input. If .omit_vals was also given, it's also ignored."
+      )
+      .value_var = NA_character_
+      .omit_vals = NA
+      warning(message)
+    }
   # If .value_var was given, make sure it exists in .df
-  if(!is.na(.value_var) & !(.value_var %in% colnames(.df))){
-    message <- paste0(
-      .value_var, " was given for .value_var, but it is not in .df! ",
-      "Ignoring input. If .omit_vals was also given, it's also ignored."
-    )
-    .value_var = NA_character_
-    .omit_vals = NA
-    warning(message)
+    if(!(.value_var %in% colnames(.df))){
+      message <- paste0(
+        .value_var, " was given for .value_var, but it is not in .df! ",
+        "Ignoring input. If .omit_vals was also given, it's also ignored."
+      )
+      .value_var = NA_character_
+      .omit_vals = NA
+      warning(message)
+    }
   }
   # Check and Lightly Process .omit_vals
   # -------------------------------------------------------------------------
   ## If .omit_vals exists but .value_var does not, revert to NA
-  if((sum(is.na(.omit_vals)) > 0) & is.na(.value_var)){
+  if((sum(!is.na(.omit_vals)) > 0) & is.na(.value_var)){
     message <- paste0(
       ".omit_vals requires .value_var, but no .value_var given. ", 
       "Ignoring passed value for .omit_vals."
@@ -276,7 +292,10 @@ get_chi_fisher <- function(
   }
   ## If .omit_vals does not contain values that exist in .value_var's values,
   ## raise warning for the user.
-  if (sum(!(.omit_vals %in% .df[[.value_var]])) > 0) {
+  if (
+    sum(!is.na(.omit_vals) > 0) & 
+    sum(!(.omit_vals %in% .df[[.value_var]])) > 0
+    ) {
     message <- paste0(
       ".omit_vals contains values which are not in .df[[.value_var]]: ",
       paste(setdiff(.omit_vals, .df[[.value_var]]), sep = ', '),
@@ -298,18 +317,26 @@ get_chi_fisher <- function(
       p = .df[[.probs_b_var]]
     ))
     fisher_vals <- suppressWarnings(stats::fisher.test(
-      .df[[.counts_a_var]], 
-      sum(.df[[.counts_a_var]], na.omit = T) * .df[[.probs_b_var]]
+      data.frame(
+        a = .df[[.counts_a_var]], 
+        b = sum(.df[[.counts_a_var]], na.omit = T) * .df[[.probs_b_var]]
+        ),
+      simulate.p.value = T
     ))
   } else {
     #### Otherwise just use given values
     chisq_vals <- suppressWarnings(stats::chisq.test(
-      .df[[.counts_a_var]], 
-      .df[[.counts_b_var]]
+      data.frame(
+        a = .df[[.counts_a_var]], 
+        b = .df[[.counts_b_var]]
+        )
     ))
     fisher_vals <- suppressWarnings(stats::fisher.test(
-      .df[[.counts_a_var]], 
-      .df[[.counts_b_var]]
+      data.frame(
+        a = .df[[.counts_a_var]], 
+        b = .df[[.counts_b_var]]
+        ),
+      simulate.p.value = T
     ))
   }
   
@@ -328,6 +355,25 @@ get_chi_fisher <- function(
         pval < 0.05 ~ "*",
         TRUE ~ ""
       )
+    ) |>
+    ### Convert numeric values to scientific notation if they'd require more
+    ### than n_decimal digits after the decimal to display
+    dplyr::mutate(
+      stat = ifelse(
+        stat < 10^(-n_decimal),
+        format(stat, digits = n_decimal + 1, scientific = T),
+        format(round(stat, n_decimal), scientific = F)
+        ),
+      param = ifelse(
+        param < 10^(-n_decimal),
+        format(param, digits = n_decimal + 1, scientific = T),
+        format(round(param, n_decimal), scientific = F)
+      ),
+      pval = ifelse(
+        pval < 10^(-n_decimal),
+        format(pval, digits = n_decimal + 1, scientific = T),
+        format(round(pval, n_decimal), scientific = F)
+      ),
     )
   
   ## If there are .omit_vals, repeat analyses with these values omitted.
@@ -346,18 +392,28 @@ get_chi_fisher <- function(
         p = omit_df[[.probs_b_var]]
       ))
       fisher_vals <- suppressWarnings(stats::fisher.test(
-        omit_df[[.counts_a_var]], 
-        sum(omit_df[[.counts_a_var]], na.omit = T) * omit_df[[.probs_b_var]]
+        data.frame(
+          a = omit_df[[.counts_a_var]], 
+          b = sum(
+            omit_df[[.counts_a_var]], na.omit = T
+            ) * omit_df[[.probs_b_var]]
+          ),
+        simulate.p.value = T
       ))
     } else {
       #### Otherwise just use given values
       chisq_vals <- suppressWarnings(stats::chisq.test(
-        omit_df[[.counts_a_var]], 
-        omit_df[[.counts_b_var]]
+        data.frame(
+          a = omit_df[[.counts_a_var]], 
+          b = omit_df[[.counts_b_var]]
+          )
       ))
       fisher_vals <- suppressWarnings(stats::fisher.test(
-        omit_df[[.counts_a_var]], 
-        omit_df[[.counts_b_var]]
+        data.frame(
+          a = omit_df[[.counts_a_var]], 
+          b = omit_df[[.counts_b_var]]
+        ),
+        simulate.p.value = T
       ))
     }
     
@@ -377,6 +433,25 @@ get_chi_fisher <- function(
           pval < 0.05 ~ "*",
           TRUE ~ ""
         )
+      ) |>
+      #### Convert numeric values to scientific notation if they'd require 
+      #### more than n_decimal digits after the decimal to display
+      dplyr::mutate(
+        stat = ifelse(
+          stat < 10^(-n_decimal),
+          format(stat, digits = n_decimal + 1, scientific = T),
+          format(round(stat, n_decimal), scientific = F)
+        ),
+        param = ifelse(
+          param < 10^(-n_decimal),
+          format(param, digits = n_decimal + 1, scientific = T),
+          format(round(param, n_decimal), scientific = F)
+        ),
+        pval = ifelse(
+          pval < 10^(-n_decimal),
+          format(pval, digits = n_decimal + 1, scientific = T),
+          format(round(pval, n_decimal), scientific = F)
+        ),
       ) |>
       #### Add method to existing rows
       dplyr::bind_rows(
